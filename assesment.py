fb7cfdb1451fa70a58f7df7e0125e6854e6b42af
khademishoja <khademishoja@gmail.com>
2023-02-26 22:05:34 +0100

    number 4

--- assesment.py
+++ assesment.py
@@ -7,3 +7,27 @@ def maxNumber(num1,num2,num3):
         print( num3 ,"is bigest number")
     return
 maxNumber(10,21,3)
+
+
+def addNumber(*nums):
+    result=sum(nums)
+    return result
+print(addNumber(2,5,10))
+
+
+def returnAge(name1,age):
+    if(age<18):
+        print("the Patient " , name1 , " is ", age , " and is underage ")
+    else:
+        print("the Patient " , name1 , " is " , age )
+
+
+
+# returnAge("mori",17)
+ 
+a = [9, 8, 10, 15, 11, 12, 14, 13,7]
+for x in a:
+    if(x>=10):
+        print(x)
+
+
